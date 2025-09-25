a75aab80c9343343e7658917bebae646161c0e18
alessandro-11 <alessandromart342@gmail.com>
2025-09-25 08:18:41 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,116 @@
+import { useState } from 'react'
+import './App.css'
+import Carmy from './assets/afnan.webp'
+import Cjoa from './assets/ameer1.webp'
+import Curs from './assets/vipblack.jpg'
+import Slider1 from './assets/perfume1.png'
+import Slider2 from './assets/perfume2.png'
+import Slider3 from './assets/slider3.jpg'
+import Slider4 from './assets/slider4.jpg'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Autoplay } from 'swiper/modules'
+import 'swiper/css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <main>
+        <header>
+          <Swiper
+            modules={[Autoplay]}
+            spaceBetween={0}
+            slidesPerView={1}
+            autoplay={{
+              delay: 3000,
+              disableOnInteraction: false,
+            }}
+            loop={true}
+            className="mySwiper"
+          >
+            <SwiperSlide>
+              <img src={Slider1} alt="Slide 1" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src={Slider2} alt="Slide 2" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src={Slider1} alt="Slide 3" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src={Slider2} alt="Slide 3" />
+            </SwiperSlide>
+          </Swiper>
+        </header>
+        <section className="produtos">
+
+          {/* Produto 1 */}
+          <a href="#" className="link-card">
+            <div className="card">
+              <img src={Cjoa} alt="" className="img-card" />
+              <h2 className="titulo-card">Al Noble Ameer</h2>
+              <p className="desc">Perfume oriental e amadeirado, com oud e âmbar, criando uma fragrância luxuosa e misteriosa.</p>
+              <p className="preco">R$ 299,99</p>
+              <div className="avaliacao"> <span>★ ★ ★ ★ ★</span> (72)</div>
+              <div className="off">- 10%</div>
+            </div>
+          </a>
+
+          {/* Produto 2 */}
+          <a href="#" className="link-card">
+            <div className="card">
+              <img src={Carmy} alt="" className="img-card" />
+              <h2 className="titulo-card">9PM Afnan</h2>
+              <p className="desc">Doce e envolvente, com frutas, âmbar e musk, perfeito para momentos especiais e intensos.</p>
+              <p className="preco">R$ 399,99</p>
+              <div className="avaliacao"> <span>★ ★ ★ ★ ☆</span> (15)</div>
+              <div className="off">- 5%</div>
+            </div>
+          </a>
+
+          {/* Produto 3 */}
+          <a href="#" className="link-card">
+            <div className="card">
+              <img src={Curs} alt="" className="img-card" />
+              <h2 className="titulo-card">212 VIP black</h2>
+              <p className="desc">Fragrância urbana e sensual, com notas de anis, lavanda, couro e baunilha, ideal para a noite.</p>
+              <p className="preco">R$ 299,99</p>
+              <div className="avaliacao"> <span>★ ★ ★ ★ ★</span> (302)</div>
+              <div className="off">- 10%</div>
+            </div>
+          </a>
+
+        </section>
+        <section className="destaque">
+
+          <iframe src="https://www.youtube.com/embed/p7cDN1ofIw0?autoplay=1&mute=1&controls=0&loop=1&playlist=p7cDN1ofIw0&modestbranding=1&showinfo=0" frameborder="0" allow="autoplay; fullscreen"></iframe>
+
+        </section>
+        
+        <footer>
+
+          <img src="" alt="" className="logo-footer" />
+
+          <div className="social-icons">
+            <div className="icon">
+              <img src="" alt="" id="instagram" />
+            </div>
+            <div className="icon">
+              <img src="" alt="" id="discord" />
+            </div>
+            <div className="icon">
+              <img src="" alt="" id="x" />
+            </div>
+            <div className="icon">
+              <img src="" alt="" id="youtube" />
+            </div>
+          </div>
+
+        </footer>
+      </main>
+    </>
+  )
+}
+
+export default App
